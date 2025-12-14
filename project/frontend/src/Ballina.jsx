1e8c2a13011ca706e795b12d79f1662486ee636a
linademiri <159142830+linademiri@users.noreply.github.com>
2025-12-14 20:56:14 +0100

    Krijimi i file-it Ballina.jsx

---
+++ project/frontend/src/Ballina.jsx
@@ -0,0 +1,24 @@
+/*import React from 'react';*/
+import Button from 'react-bootstrap/Button';
+import image1 from './assets/Ballina.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
+import HealthyLifeImage from './assets/HealthyLifeImage.png';
+import Tips1 from './assets/Tips1.png';
+import Meals from './assets/Meals.png';
+import Ending from './assets/Ending.png';
+function Ballina() {
+    return (
+        <>
+            <Image src={image1} fluid style={{ width: '100%', margin: '-1px' }} ></Image>
+            <Image src={HealthyLifeImage}  ></Image>
+            <Image src={Tips1} style={{ marginLeft: '7rem' }} ></Image>
+            <Image src={Meals} style={{ width: '95rem' }}  ></Image>
+            <Image src={Ending}  ></Image>
+
+
+        </>
+    );
+
+}
+export default Ballina;
