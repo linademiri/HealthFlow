f255a77ee51614eac4ecf80e13eb4897f517b62b
ereza3 <er69936@ubt-uni.net>
2025-12-15 17:50:11 +0100

    refactor: clean up dashboard layout component

--- project/frontend/src/Dashboard/Components/Dashboard.jsx
+++ project/frontend/src/Dashboard/Components/Dashboard.jsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { memo } from 'react';
 import Sidebar from './Sidebar';
 import Widget from './Widget';
 import MainPageStats from './MainPageStats';
 import EarningReports from './EarningReports';
 
-const Dashboard = () => {
-    return (
-        <div className="d-flex" style={{ backgroundColor: '#f5f6f7' }} >
-
-
-            <Sidebar userType="doctor" />
-
-
-            <div style={{ paddingTop:'45px' }}>
-                <MainPageStats></MainPageStats>
-            </div>
-
-            <div style={{ display: 'block', marginTop: '300px', marginLeft: '-990px' }}>
-                <EarningReports></EarningReports>
-                <br></br>
-            </div>
-        </div>
-    );
+// Inline layout constants extracted for readability and easier future tweaks
+const DASHBOARD_BACKGROUND_STYLE = { backgroundColor: '#f5f6f7' };
+const MAIN_CONTENT_STYLE = { paddingTop: '45px' };
+const EARNING_REPORTS_CONTAINER_STYLE = {
+    display: 'block',
+    marginTop: '300px',
+    marginLeft: '-990px',
 };
 
-export default Dashboard;
+const Dashboard = () => (
+    <div className="d-flex" style={DASHBOARD_BACKGROUND_STYLE}>
+        <Sidebar userType="doctor" />
+
+        <div style={MAIN_CONTENT_STYLE}>
+            <MainPageStats />
+        </div>
+
+        <div style={EARNING_REPORTS_CONTAINER_STYLE}>
+            <EarningReports />
+            <br />
+        </div>
+    </div>
+);
+
+export default memo(Dashboard);
