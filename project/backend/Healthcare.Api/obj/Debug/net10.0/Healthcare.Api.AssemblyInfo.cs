afd0153a1afc458162f69f549c4ac6ab03f09c7b
gresaklinaku05 <gk70572@ubt-uni.net>
2025-12-16 19:08:14 +0100

    Commit

--- project/backend/Healthcare.Api/obj/Debug/net10.0/Healthcare.Api.AssemblyInfo.cs
+++ project/backend/Healthcare.Api/obj/Debug/net10.0/Healthcare.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Healthcare.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b652b4f3308196de991a35c4fd08b9c0c30156b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f027ad2f4af937a4c78727ec39a76d0b154902ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Healthcare.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Healthcare.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
