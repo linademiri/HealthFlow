8e8ec7cbd7882f23ccba0385d0ae125f280b53ce
linademiri <159142830+linademiri@users.noreply.github.com>
2025-12-17 13:00:58 +0100

    Krijimi i file-ve Dhoma.cs dhe DhomaPacienti.cs

---
+++ project/backend/Healthcare.Api/Data/Models/Dhoma.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class Dhoma
+    {
+        [Key]
+        public string Id { get; set; }
+        public string NrDhomes { get; set; }
+        public string Lloji_Dhomes { get; set; }
+        public int Kapaciteti { get; set; }
+        public bool Available { get; set; }
+
+        public List<DhomaPacientit> DhomaPacienteve { get; set; }//
+
+    }
+}
---
+++ project/backend/Healthcare.Api/Data/Models/DhomaPacientit.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class DhomaPacientit
+    {
+        [Key]
+        public string Id { get; set; }
+
+        // Foreign key for Patient
+        public string PacientId { get; set; }
+        public Pacienti Pacienti { get; set; }
+
+        // Foreign key for Room
+        public string DhomaId { get; set; }
+        public Dhoma Dhoma { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
+    }
+}
