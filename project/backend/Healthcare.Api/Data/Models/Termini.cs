d36c5821bf6e958258054eda131bafbcf9a6e517
linademiri <159142830+linademiri@users.noreply.github.com>
2025-12-17 17:34:34 +0100

    Krijimi i file-ve Sherbimi dhe Termini.cs

---
+++ project/backend/Healthcare.Api/Data/Models/Termini.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Numerics;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class Termini
+    {
+        public string Id { get; set; }
+
+
+        public DateTime DataTerminit { get; set; }
+        public string Statusi { get; set; } 
+
+        public string DoktorId { get; set; }
+        public Mjeku Mjeku { get; set; }
+
+        public string PacientId { get; set; }
+        public Pacienti Pacienti { get; set; }
+    }
+}
