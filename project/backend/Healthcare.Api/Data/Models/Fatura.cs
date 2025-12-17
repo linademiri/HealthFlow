c99bbcdc1e8a11dc6f7047cd9e7b598fccb15c35
linademiri <159142830+linademiri@users.noreply.github.com>
2025-12-17 13:04:54 +0100

    Krijimi i file-ve Historiku.cs dhe Fatura.cs

---
+++ project/backend/Healthcare.Api/Data/Models/Fatura.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class Fatura
+    {
+        public string Id { get; set; }
+
+        // Foreign key for Patient
+
+        public string PacientId { get; set; }
+        [JsonIgnore]
+        public Pacienti Pacienti { get; set; }
+
+        // Foreign key for Service
+        public string SherbimiId { get; set; }
+        public Sherbimi Sherbimi { get; set; }
+
+        public decimal Shuma { get; set; }
+        public DateTime Data { get; set; }
+        public bool? Paguar { get; set; }
+    }
+}
---
+++ project/backend/Healthcare.Api/Data/Models/Historiku.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class Historiku
+    {
+        public string Id { get; set; }
+
+        public string MjekuId { get; set; }
+        public Mjeku Mjeku { get; set; }
+
+        public string PacientId { get; set; }
+        public Pacienti Pacienti { get; set; }
+
+        public DateTime Data { get; set; }
+        public string? Anamneza_Statusi { get; set; }
+        public string? Ekzaminimi { get; set; }
+        public string? Diagnoza { get; set; }
+        public string? Terapia { get; set; }
+        public string? Perfundimi { get; set; }
+
+
+    }
+}
