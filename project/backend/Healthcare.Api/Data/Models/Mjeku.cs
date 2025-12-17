00d0265dc66c8285b46929f770b13691d012cf1a
linademiri <159142830+linademiri@users.noreply.github.com>
2025-12-17 14:48:37 +0100

    Krijimi i file-ve

---
+++ project/backend/Healthcare.Api/Data/Models/Mjeku.cs
@@ -0,0 +1,19 @@
+﻿namespace ReactApp1.Server.Data.Models
+{
+    public class Mjeku
+    {
+        public string Id { get; set; }
+
+        public string Specializimi { get; set; }
+        public string NumriLicences { get; set; }
+
+        // Foreign key from User
+        public string? UserId { get; set; }
+        public User User { get; set; } 
+
+
+        public List<Termini> Terminet { get; set; }
+        public List<Historiku> Historiqet { get; set; }
+    }
+
+}
---
+++ project/backend/Healthcare.Api/Data/Models/Pacienti.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace ReactApp1.Server.Data.Models
+{
+    public class Pacienti
+    {
+        [Key]
+        public string Id { get; set; }
+        public string? UserId { get; set; }
+        public User User { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Ditelindja { get; set; }
+
+        public List<Termini> Terminet { get; set; }
+        public List<Historiku> Historiks { get; set; }
+
+        [JsonIgnore]
+        public List<Fatura> Faturat { get; set; }
+        public List<DhomaPacientit> DhomaPacienteve { get; set; }
+
+    }
+}
